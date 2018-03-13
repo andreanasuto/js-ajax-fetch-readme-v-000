9e0d9d2fd27a3a59f59234032ff4ec0b55f9a11e
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-03-13 22:53:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const token = '481f526ebc96d311991ac55b1c0589df326c6f60'
 fetch('https://api.github.com/user/repos', {
   headers: {
-    Authorization: `token ${token}`
+    'Authorization': `token ${token}`
   }
 }).then(res => res.json()).then(json => console.log(json)).done()
