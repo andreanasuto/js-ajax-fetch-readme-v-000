4beea090bceab7409ca2ec902a66b7f781cd0a6b
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-03-13 20:42:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-const app = "I don't do much.";
+const token = '481f526ebc96d311991ac55b1c0589df326c6f60'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json)).done()
